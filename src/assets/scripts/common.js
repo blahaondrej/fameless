f621fedd4c2942b88e6f35b852c5b60f4bd6047e
Ondřej Blaha <blaha@able.cz>
2024-01-06 23:27:50 +0100

    blog, team

--- src/assets/scripts/common.js
+++ src/assets/scripts/common.js
@@ -1,6 +1,44 @@
 import LocomotiveScroll from 'locomotive-scroll';
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle'
 
+// import styles bundle
+import 'swiper/swiper-bundle.css';
 /* const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true
 }); */
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+  function shortText(selektor, maxDelka) {
+    const elementy = document.querySelectorAll(selektor);
+    elementy.forEach(element => {
+      if (element.innerText.length > maxDelka) {
+        element.innerText = element.innerText.substring(0, maxDelka) + '...';
+      }
+    });
+  }
+  
+  // Použití funkce
+  shortText('.blog-item__text', 200);
